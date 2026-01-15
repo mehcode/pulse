be53e0bfe7dad52b11044995fcb5001d47b60e1e
Ryan Leckey <leckey.ryan@gmail.com>
2026-01-14 17:36:52 -0800

    chore: only use DebugAllocator in debug mode
    Signed-off-by: Ryan Leckey <leckey.ryan@gmail.com>

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,27 @@
 const std = @import("std");
+const builtin = @import("builtin");
+
 const Arguments = @import("arguments.zig").Arguments;
 const Game = @import("game.zig").Game;
 
 pub fn main() !void {
-    const gpa = generalPurposeAllocator();
+    // choose allocator based on build mode
+    // in debug mode, use the debug allocator to help catch memory issues
+    const use_debug_allocator = builtin.mode == .Debug;
 
+    if (use_debug_allocator) {
+        var debug = std.heap.DebugAllocator(.{}).init;
+        defer _ = debug.deinit();
+
+        try run(debug.allocator());
+    } else {
+        try run(std.heap.smp_allocator);
+    }
+}
+
+fn run(allocator: std.mem.Allocator) !void {
     // parse arguments from the command line
-    var args = try Arguments.parse(gpa);
+    var args = try Arguments.parse(allocator);
     defer args.deinit();
 
     // open the game ROM specified in the arguments
@@ -15,16 +30,4 @@ pub fn main() !void {
 
     std.debug.print("> PRG-ROM {} bytes\n", .{game.prg_rom.len});
     std.debug.print("> CHR-ROM {} bytes\n", .{game.chr_rom.len});
-}
-
-/// Decides on a general-purpose allocator, intended for meta allocations
-/// such as command-line argument parsing.
-fn generalPurposeAllocator() std.mem.Allocator {
-    // TODO: look into how conventionally to decide between the smp_allocator and debugAllocator
-
-    var debug = std.heap.DebugAllocator(.{
-        .thread_safe = false,
-    }){};
-
-    return debug.allocator();
 }
